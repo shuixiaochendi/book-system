6a286669c99fd5722f6150a82eff718cac9a1ee9
uatvv000689 <13917424785@163.com>
2026-04-26 10:44:44 +0800

    test

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/books1")
 @CrossOrigin(origins = "*")
 public class BookController {
 
